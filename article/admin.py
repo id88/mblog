867f2ac316fc7835e4421336c08e91e700b68e29
id88 <m82338233@163.com>
2018-12-21 11:31:08 +0800

    新增删除栏目功能

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ArticleColumn
 
-# Register your models here.
+# 使得管理员也能对文件栏目编辑和删除
+
+class ArticleColumnAdmin(admin.ModelAdmin):
+    list_display = ("column", "created", "user")
+    list_filter = ("column",)
+
+admin.site.register(ArticleColumn, ArticleColumnAdmin)
